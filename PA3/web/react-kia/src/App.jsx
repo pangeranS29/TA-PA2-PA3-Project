1c61836f7314dedb26eec995ddbee172e373cb17
SamsonSitorus <149450441+SamsonSitorus@users.noreply.github.com>
2026-04-13 20:42:25 +0700

    layout

--- PA3/web/react-kia/src/App.jsx
+++ PA3/web/react-kia/src/App.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import NeonatusList from "./pages/Neonatus";
+import CreateNeonatus from "./pages/Neonatus/Create";
 import { isAuthenticated } from "./services/auth";
 
 const PrivateRoute = ({ children }) => {
@@ -14,14 +16,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
+        <Route path="/dashboard" element={ <PrivateRoute><Dashboard /></PrivateRoute> }/>
+        <Route path="/neonatus" element={ <PrivateRoute><NeonatusList /></PrivateRoute> }/>
+        <Route path="/neonatus/create" element={ <PrivateRoute><CreateNeonatus /></PrivateRoute> }/>
         <Route path="/" element={<Navigate to="/dashboard" />} />
         {/* Tambahkan route lain jika perlu */}
       </Routes>
