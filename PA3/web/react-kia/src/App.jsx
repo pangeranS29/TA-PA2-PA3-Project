cdd15586e2995d579937a7271844da6f2364d3d9
vSAM100904v <149343649+vSAM100904v@users.noreply.github.com>
2026-04-14 20:47:56 +0700

    Revert "Merge branch 'feature/PA-3_Dashboard_Monitoring' of https://github.com/pangeranS29/TA-PA2-PA3-Project into feature/PA-3_Dashboard_Monitoring"
    This reverts commit 42012a2847091cfb94c22defd33624b8ccd88890, reversing
    changes made to 1f29b0a725b56ca6ef8f9d0ee3e9ecb3986912a2.

--- PA3/web/react-kia/src/App.jsx
+++ PA3/web/react-kia/src/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import NeonatusList from "./pages/Neonatus";
-import CreateNeonatus from "./pages/Neonatus/Create";
 import { isAuthenticated } from "./services/auth";
 import DataIbu from "./pages/DataIbu";
 
@@ -16,9 +14,14 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={ <PrivateRoute><Dashboard /></PrivateRoute> }/>
-        <Route path="/neonatus" element={ <PrivateRoute><NeonatusList /></PrivateRoute> }/>
-        <Route path="/neonatus/create" element={ <PrivateRoute><CreateNeonatus /></PrivateRoute> }/>
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
         <Route path="/" element={<Navigate to="/dashboard" />} />
         {/* Tambahkan route lain di sini jika perlu */}
         <Route path="*" element={<Navigate to="/dashboard" />} />
