e0d9c28eab17c0d1fdbeb4bfb17fc9824f9e0404
randominize_m <mangaturJeremiS@Gmail.com>
2026-04-14 10:45:40 +0700

    feat : models, seeder, get(kehamilan), get(riwyat anc), post(data anc)

---
+++ backend/monolith/go-template-main/cmd/seeder/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"monitoring-service/app/models"
+	"monitoring-service/app/seeders"
+	"monitoring-service/pkg/config"
+
+	"github.com/spf13/viper"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// 1. Inisialisasi Viper
+	viper.SetConfigFile(".env")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Printf("Masalah .env : %v", err)
+	}
+
+	cfg := config.NewConfig()
+
+	pg := cfg.Postgres().Write
+	
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Jakarta",
+		pg.URL, pg.Username, pg.Password, pg.Name, pg.Port)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Gagal koneksi database : ", err)
+	}
+
+	err = db.AutoMigrate(
+		&models.Role{},
+		&models.User{},
+		&models.Kependudukan{},
+		&models.Ibu{},
+		&models.Kehamilan{},
+		&models.RiwayatKehamilan{},
+		&models.PemeriksaanANC{},
+	)
+	if err != nil {
+		log.Fatal("Migrasi Gagal : ", err)
+	}
+	if err := seeders.RunAllSeeders(db); err != nil {
+		log.Fatal("Seeding Gagal : ", err)
+	}
+
+	log.Println("Berhasil seed ...")
+}
