256d599e94982bc8e4a23aa7a649be25b06fe208
Jonathantmb12 <primatmb12@gmail.com>
2026-04-30 17:41:00 +0700

    pelayanan nifas

--- backend/monolith/go-template-main/app/models/catatan_pelayanan_nifas.go
+++ backend/monolith/go-template-main/app/models/catatan_pelayanan_nifas.go
@@ -1,16 +1,17 @@
+// models/catatan_pelayanan_nifas.go
 package models
 
 import "time"
 
 type CatatanPelayananNifas struct {
-	IDCatatanNifas                  int32      `gorm:"primaryKey" json:"id_catatan_nifas"`
-	KehamilanID                     int32      `gorm:"not null;index" json:"kehamilan_id"`
-	Kehamilan                       *Kehamilan `gorm:"foreignKey:KehamilanID;references:ID" json:"kehamilan,omitempty"`
-	TanggalPeriksaStampParaf        *time.Time `gorm:"type:date" json:"tanggal_periksa_stamp_paraf"`
-	KeluhanPemeriksaanTindakanSaran string     `json:"keluhan_pemeriksaan_tindakan_saran"`
-	TanggalKembali                  *time.Time `gorm:"type:date" json:"tanggal_kembali"`
+    IDCatatanNifas                  int32      `gorm:"primaryKey;autoIncrement" json:"id_catatan_nifas"`
+    KehamilanID                     int32      `json:"kehamilan_id"`
+    KunjunganKe                     string     `json:"kunjungan_ke"`
+    TanggalPeriksaStampParaf        *time.Time `json:"tanggal_periksa_stamp_paraf"`
+    KeluhanPemeriksaanTindakanSaran string     `json:"keluhan_pemeriksaan_tindakan_saran"`
+    TanggalKembali                  *time.Time `json:"tanggal_kembali"`
 }
 
 func (CatatanPelayananNifas) TableName() string {
-	return "catatan_pelayanan_nifas"
+    return "catatan_pelayanan_nifas"
 }
