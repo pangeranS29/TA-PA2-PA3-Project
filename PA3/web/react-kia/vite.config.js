7fc2902c47f942d27b9527505b64dcbaed67c723
Jonathantmb12 <primatmb12@gmail.com>
2026-06-01 00:47:17 +0700

    Remove Rolldown plugin

--- PA3/web/react-kia/vite.config.js
+++ PA3/web/react-kia/vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
+    react()
   ],
   optimizeDeps: {
     exclude: [
       'react-chartjs-2',
       'chart.js',
-      'react-select'
+      'react-select',
+      'jspdf',
+      'html2canvas'
     ]
   }
 })
